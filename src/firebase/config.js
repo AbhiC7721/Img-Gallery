06fbb7cfeae5809e88353f664a08dc91257dc997
AbhiC7721 <abhic7721@gmail.com>
2021-03-23 21:38:01 +0530

    Firebase configured and upload form created

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase/app";
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration 
+var firebaseConfig = {
+    apiKey: "AIzaSyCrJU60OSNr6or34u42T5xPMb48YnWPlw0",
+    authDomain: "abhi-gallery.firebaseapp.com",
+    projectId: "abhi-gallery",
+    storageBucket: "abhi-gallery.appspot.com",
+    messagingSenderId: "482916208211",
+    appId: "1:482916208211:web:898fb7887185a816a280a6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFireStore = firebase.firestore();
+
+  export { projectStorage, projectFireStore };
